8dbac58eb38b7334ad29345f496f919b13642242
Matias M <matiasmasca@gmail.com>
2015-06-24 10:57:15 -0300

    Arrancando el BDD

---
+++ features/step_definitions/identificacion_steps.rb
@@ -0,0 +1,19 @@
+Dado(/^que soy cliente registrado$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Dado(/^me identifico como "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Entonces(/^veo un mensaje "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Dado(/^que el usuario esta "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Entonces(/^veo un mensaje que dice "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
