fd3a99ce817a63f912c36183daaec28b5b29598e
Matias M <matiasmasca@gmail.com>
2015-07-31 11:56:17 -0300

    configurando el host en deploy paraaction mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,10 +13,12 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' } #para Devise
+ config.action_mailer.default_url_options = { :host => 'localhost:3000' } #para Devise
 
-  config.action_mailer.raise_delivery_errors = false
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
   #config.action_mailer.perform_deliveries = true #Para que envie, por defecto es false.
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
