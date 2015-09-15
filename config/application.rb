7080ecbc1469799cfd20e405af1f401792b58a1d
Matias M <matiasmasca@gmail.com>
2015-09-15 19:19:23 -0300

    Deploy VIP

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module NovedadesRor
     config.active_record.raise_in_transactional_callbacks = true
 
     # OJO con esto, ya que esta permitiendo que se usen iFrame.
-    config.action_dispatch.default_headers = { 'X-Frame-Options' => 'ALLOW-FROM http://terciar.ga/' }
+    config.action_dispatch.default_headers = { 'X-Frame-Options' => 'ALLOW-FROM http://terciar.info/' }
 
   end
 end
