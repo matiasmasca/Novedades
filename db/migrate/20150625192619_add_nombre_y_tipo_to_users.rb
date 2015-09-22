e5bb3207c2a2608bde9d638a69d6b453e5e65517
Matias M <matiasmasca@gmail.com>
2015-09-22 20:32:49 -0300

    comentario

--- db/migrate/20150625192619_add_nombre_y_tipo_to_users.rb
+++ db/migrate/20150625192619_add_nombre_y_tipo_to_users.rb
@@ -3,5 +3,6 @@ class AddNombreYTipoToUsers < ActiveRecord::Migration
     add_column :users, :nombre, :string
     add_column :users, :tipo, :integer, default: 2, null: false #2= Cliente.
     add_column :users, :habilitado, :boolean, default: true
+    #add_column :users, :telefono_movil, :string
   end
 end
